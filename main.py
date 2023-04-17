a2b50be634abdc59cbf26a9645432334d0dd1b8a
Mohammed Farhan Hussain <farhan.h@phalanxconsultancy.com>
2023-04-17 15:14:12 +0530

    added dotenv file

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI ,HTTPException
+from dotenv import load_dotenv
+load_dotenv()
+import os
 from pymongo import MongoClient
 import yfinance as yf
+from datetime import datetime
 from bson import ObjectId
-from datetime import datetime, timedelta
 from fastapi.middleware.cors import CORSMiddleware
 from apscheduler.schedulers.background import BackgroundScheduler
 import requests
 
+api_url = os.getenv('API')
 app = FastAPI()
 
 app.add_middleware(
@@ -17,7 +21,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient(os.getenv("MONGO"))
 db = client["finance"]
 collection = db["stocks"]
 
@@ -25,15 +29,20 @@ collection = db["stocks"]
 @app.get("/fetch_stocks/{symbol}")
 def fetch_data(symbol: str):
     stock = yf.Ticker(symbol)
-    data = stock.history(period="max")
-    if data.empty:
-        raise HTTPException(status_code=400, detail=f"No data exists for {symbol}. Try with these tickers: AAPL, META, TSLA")
-    data = data.reset_index().to_dict("records")
     collection = db[symbol]
+    last_record = collection.find_one(sort=[("Date", -1)])
+    if last_record:
+        last_date = last_record["Date"].strftime("%Y-%m-%d")
+        data = stock.history(start=last_date)
+    else:
+        data = stock.history(period="max")
+    if data.empty:
+        raise HTTPException(status_code=400, detail=f"No new data exists for {symbol}. Try with these tickers: AAPL, META, TSLA")
+    data = data.reset_index().to_dict("records")
     collection.insert_many(data)
-    return {"message": f"{len(data)} records fetched and saved into {symbol} collection."}
+    return {"message": f"{len(data)} new records fetched and saved into {symbol} collection."}
 
-# This end point will sent stocks data from database to frontEnd
+# This end point will sent stocks data from our database to frontEnd
 @app.get("/get_stocks/{symbol}")
 def get_data(symbol: str):
     collection = db[symbol]
@@ -50,25 +59,25 @@ def get_collections():
     stocks = db.list_collection_names()
     return {"stocks": stocks}
 
-
+# scheduler to fetch data and save in database
 scheduler = BackgroundScheduler()
 
 def fetch_stocks(symbols: list):
     for symbol in symbols:
-        url = f"http://localhost:8000/fetch_stocks/{symbol}"
+        url = f"{api_url}/fetch_stocks/{symbol}"
         response = requests.get(url)
         print(response.json())
 
 def fetch_stocks_dynamic():
-    # Get the list of tickers from the endpoint
-    tickers_url = "http://localhost:8000/stocks_in_db" 
+    # Get the list of tickers from our database
+    tickers_url = f"{api_url}/stocks_in_db" 
     tickers_response = requests.get(tickers_url)
     tickers_data = tickers_response.json()
     symbols = tickers_data["stocks"]
 
-    # Fetch data for each ticker
+    # Fetch ticker data for each in our database
     for symbol in symbols:
-        url = f"http://localhost:8000/fetch_stocks/{symbol}"
+        url = f"{api_url}/fetch_stocks/{symbol}"
         response = requests.get(url)
         print(response.json())
 
